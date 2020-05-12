23c1602da324da0f3a2b04738d252c7cbd4358bf
Peter Simpson <simpson.petere@gmail.com>
2020-05-12 18:02:41 -0400

    updated class header

--- latinsquare.h
+++ latinsquare.h
@@ -55,6 +55,9 @@ private:
 	*/
 	bool isNewValid(unsigned row, unsigned col);
 
+	//recursive back-tracking helper function for solve(), performs most of the work in solving
+	bool solveHelp(std::set<T>& contents, int row, int col){
+
 	/*
 	* Checks the validity of the entire Latin square
 	* Worst Case Runtime: O((n^2)logn)
